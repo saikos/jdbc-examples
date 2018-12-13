70e1bd1c930f1375ce303493b2264450f5db5f25
Kostas Saidis <saikos@gmail.com>
2018-12-13 10:31:04 +0200

    Add PreparedStatement examples

--- src/org/afdemp/cb6/jdbc/SimpleExample.java
+++ src/org/afdemp/cb6/jdbc/SimpleExample.java
@@ -3,13 +3,15 @@ package org.afdemp.cb6.jdbc;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 /*
  This example demonstrates the usage of the primary 
- JDBC API classes (Connection, Statement, ResultSet) 
- that are required to fetch data from a database.
+ JDBC API classes (DriverManager, Connection, Statement, 
+ PreparedStatement, ResultSet) that are required to 
+ manipulate data held in a database.
 */
 public class SimpleExample {
     
@@ -30,13 +32,39 @@ public class SimpleExample {
             String password = "eshopAdmin123";
             
             Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/eshop?characterEncoding=utf-8&autoReconnect=true",
+                "jdbc:mysql://localhost:3306/eshop?serverTimezone=UTC&characterEncoding=utf-8&autoReconnect=true",
                 user,
                 password
-            );
+            );                       
             
-            Statement stmt = con.createStatement();
+            String newFullName = "Πασπαράκης, Γιώργος";
+            String newEmail = "geospa@cb6";
             
+            /*
+            NEVER, EVER (EVER) DO THIS                                    
+            String sqlUpdate = "update user " +
+                               "set fullname = '" + newFullName + "', " +
+                               "email = '" + newEmail + "' " +
+                               "where id = 6";                                
+            
+            int cnt = stmt.executeUpdate(sqlUpdate);
+            System.out.println(cnt);
+            */
+            
+            //Always use PreparedStatement to execute 
+            //dynamic / parameterized queries
+            String sqlUpdate = "update user set fullname = ?, email = ? where id = 6";            
+            PreparedStatement ps = con.prepareStatement(sqlUpdate);
+            
+            ps.setString(1, newFullName);
+            ps.setString(2, newEmail);
+            
+            int cnt = ps.executeUpdate();
+            System.out.println("Lines afffected: " + cnt);
+            
+            //You can use Statement to execute "static" queries
+            //(non-dynamic, ones that accept no parameters)
+            Statement stmt = con.createStatement();        
             ResultSet rs = stmt.executeQuery("SELECT * from user order by id desc");                        
             
             while(rs.next()) {
@@ -46,6 +74,21 @@ public class SimpleExample {
                 
                 System.out.println("User: " + id + ", " + email + ", " + fName);
             }                        
+            
+            //Finally, you can use PreparedStatement
+            //to execute the "static" queries, too
+            PreparedStatement ps2 = con.prepareStatement("select * from product");
+            ResultSet rs2 = ps2.executeQuery();
+            while(rs2.next()) {
+                Long id      = rs2.getLong(1);
+                String name  = rs2.getString(2);
+                Double price = rs2.getDouble(3);
+                        
+                System.out.println("Product: " + id + ", " + name + ", " + price);       
+            }  
+            
+            //Always use PreparedStatement for
+            //accessing a database, if possible :)
         }
         catch(Throwable t) {
             System.out.println(t.getMessage());
